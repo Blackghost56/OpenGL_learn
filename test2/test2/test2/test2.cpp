01bb5ff948b1d2c9e9402dc8795298428d08d13b
Tsvetkov Michael <blackghost0702@gmail.com>
2018-12-14 17:56:42 +0300

    test2 edit

--- test2/test2/test2/test2.cpp
+++ test2/test2/test2/test2.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <string>
+#include <iostream>
 
 
 float array_color[3][3] = { { 1.0, 0.0, 0.0 }, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0} };
@@ -15,6 +16,15 @@ DWORD time = 0;
 int count_frame = 0;
 std::string count_frame_str = "";
 int h, w;
+
+
+GLint viewport[4];
+GLdouble modelview[16];
+GLdouble projection[16];
+
+GLdouble wx, wy, wz;
+
+GLfloat zval;
 
 
 void renderBitmapString(float x, float y, float z, void *font, std::string &string)
@@ -48,7 +58,7 @@ void changeSize(int ww, int hh) {
 	glViewport(0, 0, ww, hh);
 
 	// установить корректную перспективу.
-	gluPerspective(45, ratio, 1, 1000);
+	gluPerspective(45, ratio, 1, 100);
 
 	// вернуться к модели
 	glMatrixMode(GL_MODELVIEW);
@@ -59,26 +69,6 @@ void changeSize(int ww, int hh) {
 				0.0f, 0.0f, 0.0f,
 				0.0f, 1.0f, 0.0f);*/
 
-}
-
-
-void setOrthographicProjection() {
-	//переключения режима проецирования
-	glMatrixMode(GL_PROJECTION);
-	//Сохраняем предыдущую матрицу, которая содержит
-		//параметры перспективной проекции
-	glPushMatrix();
-	//обнуляем матрицу
-	glLoadIdentity();
-	//устанавливаем 2D ортографическую проекцию
-	gluOrtho2D(0, w, 0, h);
-	//перевернём ось y, положительное направление вниз
-	glScalef(1, -1, 1);
-	// Движение происходит из левого нижнего угла
-	// В верхний левый угол
-	glTranslatef(0, -h, 0);
-	// возврата в режим обзора модели
-	glMatrixMode(GL_MODELVIEW);
 }
 
 
@@ -93,35 +83,52 @@ void renderScene(void) {
 		count_frame_str = "FPS: " + std::to_string(count_frame);
 		time = GetTickCount();
 		count_frame = 0;
+		//std::cout << count_frame_str << std::endl;
 	}
 	// --------------
 
 
 	glLoadIdentity();
+
+	// Преобразование кординат из глобальных в экранные
+/*
+glGetIntegerv(GL_VIEWPORT, viewport);
+glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+glGetDoublev(GL_PROJECTION_MATRIX, projection);
+gluProject(1.0, 0.0, 0.0, modelview, projection, viewport, &wx, &wy, &wz);
+*/
+//
+// 
+	/* Из экранных в глобальные
+	glGetIntegerv(GL_VIEWPORT, viewport);
+	glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+	glGetDoublev(GL_PROJECTION_MATRIX, projection);
+
+	glReadPixels(371, 250, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &zval);
+
+	gluUnProject(371, 250, zval, modelview, projection, viewport, &wx, &wy, &wz);
+	*/
+	//
+
+
+
 	// установка камеры
 	gluLookAt(0.0f, 0.0f, 5.0f,
 		0.0f, 0.0f, 0.0f,
 		0.0f, 1.0f, 0.0f);
 
 
-	glRotatef(0.0, 0.0f, 1.0f, 0.0f);
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glBegin(GL_QUADS);
-
+	
 	glColor3f(0.5, 0.5, 0.5);
+
 	glVertex3f(1.0, 0.0, 0.0);
-
-	glColor3f(0.5, 0.5, 0.5);
 	glVertex3f(2.0, 0.0, 0);
-
-	glColor3f(0.5, 0.5, 0.5);
 	glVertex3f(2.0, 1.0, 0);
-
-	glColor3f(0.5, 0.5, 0.5);
 	glVertex3f(1.0, 1.0, 0);
-
+	
 	glEnd();
 
 	std::string start_str = "Start";
@@ -147,6 +154,8 @@ void renderScene(void) {
 	glVertex3f(0.5, -0.5, 0);
 
 	glEnd();
+
+
 
 
 	angle += 0.1f;
@@ -184,27 +193,54 @@ void processSpecialKeys(int key, int x, int y) {
 }
 
 
+
+
+
 void mouseButton(int button, int state, int x, int y) {
  
 	// только при начале движения, если нажата левая кнопка
 	if (button == GLUT_LEFT_BUTTON) {
- 
-		// когда кнопка отпущена
 		if (state == GLUT_DOWN) {
-			count_frame_str = "x= " + std::to_string(x) + "   y= " + std::to_string(y);
+			//count_frame_str = "x= " + std::to_string(x) + "   y= " + std::to_string(y);
+			std::cout << "x= " + std::to_string(x) + "   y= " + std::to_string(y) << std::endl;
 		}
 	}
+
+	if (button == GLUT_RIGHT_BUTTON) {
+		if (state == GLUT_DOWN) {
+
+			//glPushMatrix();
+			glLoadIdentity();
+			glGetIntegerv(GL_VIEWPORT, viewport);
+			glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+			glGetDoublev(GL_PROJECTION_MATRIX, projection);
+			//glPopMatrix();
+
+			glReadPixels(x, y, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &zval);
+
+			gluUnProject(x, y, zval, modelview, projection, viewport, &wx, &wy, &wz);
+
+			std::cout << "x= " + std::to_string(wx) + "   y= " + std::to_string(wy) + "   z= " + std::to_string(wz) << std::endl;
+
+			//std::cout << "x= " + std::to_string(wx) + "   y= " + std::to_string(wy) + "   z= " + std::to_string(wz) << std::endl;
+		}
+	}
+}
+
+void Timer(int)
+{
+
+	glutPostRedisplay();
+	glutTimerFunc(40, Timer, 0);
 }
 
 
 int main(int argc, char **argv) {
 
-
-
 	// инициализация
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(200, 200);
+	glutInitWindowPosition(800, 200);
 	glutInitWindowSize(500, 500);
 	glutCreateWindow("Test_2");
 
@@ -218,6 +254,8 @@ int main(int argc, char **argv) {
 
 	// новая функция для регистрации
 	glutIdleFunc(renderScene);
+
+	//glutTimerFunc(40, Timer, 0);//анимация таймер ф-я нужна в милисек.
 
 	// Keyboard
 	glutKeyboardFunc(processNormalKeys);
